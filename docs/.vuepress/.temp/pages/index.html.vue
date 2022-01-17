34c156e5cd1e5d6ca3573400eb199a88a140761e
maxnth <maximilian.noeth@protonmail.com>
2022-01-17 12:22:40 +0100

    add documentation scaffolding

---
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,2 @@
+<template><h1 id="semantic-mediawiki-docker" tabindex="-1"><a class="header-anchor" href="#semantic-mediawiki-docker" aria-hidden="true">#</a> Semantic MediaWiki Docker</h1>
+</template>
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Semantic MediaWiki Docker
