26e7433e38c6c4395e9996952334dfd62ce7c222
maxnth <maximilian.noeth@protonmail.com>
2024-03-24 23:13:11 +0100

    remove docs

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="semantic-mediawiki-docker" tabindex="-1"><a class="header-anchor" href="#semantic-mediawiki-docker" aria-hidden="true">#</a> Semantic MediaWiki Docker</h1>
-</template>
