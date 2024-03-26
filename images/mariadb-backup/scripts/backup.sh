177d3c806a7896a8b9d37eaf99984a58e3441e77
maxnth <maximilian.noeth@protonmail.com>
2024-03-26 15:13:10 +0100

    mariadb-backup: update Dockerfile

--- images/mariadb-backup/scripts/backup.sh
+++ images/mariadb-backup/scripts/backup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 DATE=$(date +%F)
-mariadb-dump --user=$DB_USER --password=$DB_PASSWORD --lock-tables --databases $DB_NAME > /backup/$DATE/db_dump.sql
+mkdir -p /backup/${DATE}
+mariadb-dump --user=$DB_USER --password=$DB_PASSWORD --lock-tables --databases $DB_NAME > /backup/${DATE}/db_dump.sql
