97b3eca85a42b088da93e9605220494720a994d8
maxnth <maximilian.noeth@protonmail.com>
2024-04-02 15:31:03 +0200

    custom: fix xml dump script

--- images/custom/scripts/dumpBackup.sh
+++ images/custom/scripts/dumpBackup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DATE=$(date +%F)
 mkdir -p /backup/${DATE}
-/var/www/html/maintenance php dumpBackup.php --full --quiet > /backup/${DATE}/dump.xml
+php /var/www/html/maintenance/dumpBackup.php --full --quiet > /backup/${DATE}/dump.xml
