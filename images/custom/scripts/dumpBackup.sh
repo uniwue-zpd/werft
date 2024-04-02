b5742a1e19e2059b49ca778056ea599f763dc604
maxnth <maximilian.noeth@protonmail.com>
2024-04-02 20:48:25 +0200

    custom: update dumpBackup.sh

--- images/custom/scripts/dumpBackup.sh
+++ images/custom/scripts/dumpBackup.sh
@@ -2,4 +2,4 @@
 DATE=$(date +%F)
 mkdir -p /backup/${DATE}
 echo "[$(date +%T)] Dumping XML backup into /backup/${DATE}/dump.xml" >> /wiki-debug.log
-/usr/local/bin/php /var/www/html/maintenance/dumpBackup.php --full --quiet > /backup/${DATE}/dump.xml 2>&1
+/usr/local/bin/php /var/www/html/maintenance/dumpBackup.php --full --quiet > /backup/${DATE}/dump.xml
